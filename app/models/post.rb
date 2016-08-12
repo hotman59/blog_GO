699326bf06d94f2b1562fdc87916eb3fd48aa133
othman <benrahmaneothman@gmail.com>
2016-08-12 11:21:18 +0200

    add button destroy comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates_presence_of :title, presence: true
   validates_presence_of :body, presence: true
 end
